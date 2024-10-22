4f152f9023608babe6ab12faee781f9bea96f7b2
Zvone <zvone.kozull1999@gmail.com>
2024-10-22 19:38:21 +0200

    Dodani gumbi na kalkulator i on radi osnovne matematičke funkcije

--- App.js
+++ App.js
@@ -1,23 +1,51 @@
-import React from "react";
 import React, {useState} from "react";
 
 function App() {
     const [input, setInput] = useState (""); //Početno stanje unosa
+    const [result, setResult] = useState("");
 
     //Funkcija dodavanje brojeva u unos.
     const handleInput = (value) => {
         setInput (input + value);
     };
-
     //Funkcija za izračun rezultata.
     const calculateResult = () => {
         try {
-            setInput(eval(input));
+            setResult(eval(input));
+            setInput(eval(input).toString());   //Prikaz rezultata na ekranu
         } catch (error){;
         setInput("Mistake");
         }
     };
+    const clearInput = () => {
+        setInput("");
+        setResult("");
+    };
 
+return (
+    <div>
+        <h1>Calculator</h1>
+        <input type="text" value={input} readOnly/>
+        <div> 
+            <button onClick={calculateResult}>=</button>
+            <button onClick={clearInput}>C</button><br></br>
+            <button onClick={() => handleInput("1")}>1</button>
+            <button onClick={() => handleInput("2")}>2</button>
+            <button onClick={() => handleInput("3")}>3</button><br></br>
+            <button onClick={() => handleInput("4")}>4</button>
+            <button onClick={() => handleInput("5")}>5</button>
+            <button onClick={() => handleInput("6")}>6</button><br></br>
+            <button onClick={() => handleInput("7")}>7</button>
+            <button onClick={() => handleInput("8")}>8</button>
+            <button onClick={() => handleInput("9")}>9</button><br></br>
+            <button onClick={() => handleInput("0")}>0</button><br></br>
+            <button onClick={() => handleInput("+")}>+</button>
+            <button onClick={() => handleInput("-")}>-</button>
+            <button onClick={() => handleInput("*")}>*</button>
+            <button onClick={() => handleInput("/")}>/</button>
+            </div>
+    </div>
+); 
 }
 
 export default App;
